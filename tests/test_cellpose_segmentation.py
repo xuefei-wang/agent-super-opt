d8ed7758fd4e2deaae7774f6d7781d3a9ca05f72
Alex Farhang <afarhang@caltech.edu>
2025-04-17 20:13:31 +0000

    update test

--- tests/test_cellpose_segmentation.py
+++ tests/test_cellpose_segmentation.py
@@ -12,7 +12,7 @@ from numpy.testing import assert_array_equal
 
 class TestCellposeSegmentation(unittest.TestCase):
     # Class variables to store command line arguments
-    data_path = '/home/alexfarhang/data/cellpose/train/'
+    data_path = '/home/alex/data/cellpose/my_split/val_set/'
     device = 1
     model_name = "cyto3"
     num_files = 8
@@ -21,39 +21,33 @@ class TestCellposeSegmentation(unittest.TestCase):
         '''Test if prediction and evaluation pipeline completes without errors'''
         load_dotenv()
 
-        cellpose_tool = CellposeTool(model_name=self.model_name, device=self.device)
-        f_path = self.data_path
-        files = sorted(glob.glob(f_path + '*_img.png'))[:self.num_files]
-        raw_images = [imread(f) for f in files]
-
-        gt_masks = [imread(f.split('.')[0][:-3] + 'masks' + '.' + f.split('.')[1]) for f in files]
-        gt_masks = [np.expand_dims(mask, axis=2) for mask in gt_masks]
+        segmenter = CellposeTool(model_name='cyto3', device=self.device)
+        raw_images, gt_masks = segmenter.loadData(self.data_path)
+        raw_images = raw_images[:self.num_files]
+        gt_masks = gt_masks[:self.num_files]
 
         images = ImageData(raw=raw_images,
                         batch_size=8,
-                        image_ids=[i for i in range(self.num_files)],
-                        masks=gt_masks)
+                        image_ids=[i for i in range(self.num_files)]) 
         
         def preprocess_images(images: ImageData) -> ImageData:
             return images
 
         images = preprocess_images(images)        
 
-        pred_masks, flows, styles, imgs = cellpose_tool.predict(images, batch_size=images.batch_size)
-        # gt_masks_list = [images.masks[i] for i in range(images.masks.shape[0])] 
-        metrics, losses = cellpose_tool.evaluate(pred_masks, images.masks)
+        pred_masks = segmenter.predict(images, batch_size=images.batch_size)
+        metrics, losses = segmenter.evaluate(pred_masks, gt_masks)
         print(metrics)
         print(losses)
             
         self.assertIn('average_precision', metrics)
         self.assertIn('bce_loss', losses)
-        # assert_array_equal(metrics['average_precision'], np.array([0.9464578, 0.8065034, 0.3700052], dtype=np.float32))
-        # self.assertEqual(losses['bce_loss'], 0.6078091263771057)
+   
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Run Cellpose segmentation tests with custom parameters')
-    parser.add_argument('--data_path', type=str, default='/home/alexfarhang/data/cellpose/train/',
+    parser.add_argument('--data_path', type=str, default='/home/alex/data/cellpose/my_split/val_set/',
                       help='Path to the dataset directory')
     parser.add_argument('--device', type=int, default=1,
                       help='GPU ID to use.')
