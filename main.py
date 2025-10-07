209e9cbc83dd61f5137201144d38663f9266373b
xuefei-wang <wang.xuefei.wang@gmail.com>
2025-10-06 18:06:28 -0700

    reduce #iters & revert back to gpt4

--- main.py
+++ main.py
@@ -421,10 +421,10 @@ def main(args: argparse.Namespace):
 
     # Configuration
     cache_seed = 4 # Cache seed for caching the results
-    num_optim_iter = 40 # Number of optimization iterations
+    num_optim_iter = 20 # Number of optimization iterations
     max_round = 20  # Maximum number of rounds for the conversation
     checkpoint_path = args.checkpoint_path
-    llm_model = "gpt-5" # Do not modify this string
+    llm_model = "gpt-4.1" # Do not modify this string
     
     # Load task prompts
     if args.experiment_name == "spot_detection":
